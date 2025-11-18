171ec7baa0618b03fd5e0adbe04dfcb60eceed59
rsgill1978 <37455001+rsgill1978@users.noreply.github.com>
2025-11-18 15:14:36 -0500

    Add files via upload

---
+++ 02/DataTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Neater way to declare variables en masse
+            int x = 5,
+                y = 6,
+                z = 7;
+            
+            int age = 23;
+            Console.WriteLine(age);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+
+            long bigNumber = 90000000L;
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+
+            double negative = -55.2D;
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+            float precision = 5.000001F;
+            Console.WriteLine(precision);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+
+            decimal money = 5.99M;
+            Console.WriteLine(money);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03/String_Chars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace String_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Gill"; // Note the value must be enclosed within " NOT '
+            char letter = 'a'; //Note the value must be enclosed within ' NOT "
+
+            Console.WriteLine(name);
+            Console.WriteLine(letter);
+
+            Console.Write("My name is: ");
+            Console.Write(name);
+            Console.WriteLine();
+            Console.WriteLine("The character is: " + letter);
+
+            Console.ReadLine();
+        }
+    }
+}
